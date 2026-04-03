60b5e06609c3590dc956f40fab51f52bdf11c4ee
chitnupong <chitnupong2548@gmail.com>
2026-04-03 21:47:59 +0700

    Config server src/index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 
 const app = express();
-
 const port = process.env.PORT || 3000;
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('API Server is running!');
